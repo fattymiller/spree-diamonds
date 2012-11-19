29c0867e9fd89780d043d7088c004b6783882248
Fatty <fattymiller@gmail.com>
2012-11-19 18:37:34 +1000

    showing converted price on product maintenance

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -1,4 +1,11 @@
 Spree::Product.class_eval do
-  attr_accessible :is_in_usd
+  attr_accessible :is_in_usd, :unconverted_price
   delegate_belongs_to :master, :is_in_usd
+  
+  def unconverted_price=(unconverted)
+    self.master.price = unconverted
+  end
+  def unconverted_price
+    self.master[:price]
+  end
 end
--- app/overrides/add_in_usd_flag_to_variant.rb
+++ app/overrides/add_in_usd_flag_to_variant.rb
@@ -1,11 +1,20 @@
 Deface::Override.new(:virtual_path => "spree/admin/products/_form",
+                     :name => "change_master_price_target",
+                     :replace => "code[erb-loud]:contains('f.text_field :price')",
+                     :disabled => false,
+                     :text => "<%= f.text_field :unconverted_price, :value => number_to_currency(@product.unconverted_price, :unit => '') %>")
+ 
+Deface::Override.new(:virtual_path => "spree/admin/products/_form",
                      :name => "add_in_usd_to_variant",
-                     :insert_top => "[data-hook=admin_product_form_right]",
+                     :insert_after => "code[erb-loud]:contains('f.error_message_on :price')",
                      :disabled => false,
                      :text => "
-       <%= f.field_container :is_in_usd do %>
-         <%= f.label :is_in_usd, 'Price is in USD?' %><br />
-         <%= f.check_box :is_in_usd %>
-         <%= f.error_message_on :is_in_usd %>
-       <% end %>
- ")
+     <%= f.field_container :is_in_usd do %>
+       <%= f.check_box :is_in_usd, :style => 'margin-left: 20px;', :'data-rate' => Spree::Config.conversion_rate %>
+       <%= f.label :is_in_usd, 'Price is in USD?' %>
+       
+       <p id=\"aud_display\" style=\"margin-left: 20px;\">
+        <strong>AUD </strong> <span><%= number_to_currency @product.master.price %></span> (1 <strong>US dollar</strong> = <%= Spree::Config.conversion_rate %> <strong><%= Spree::Config.conversion_rate == 1 ? \"Australian dollar\" : \"Australian dollars\" %></strong>)
+       </p>
+     <% end %>
+")
