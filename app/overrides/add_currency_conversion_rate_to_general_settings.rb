f68a94fbe105c6fbb838676cacc8755a07cee98f
Fatty <fattymiller@gmail.com>
2012-11-19 13:51:05 +1000

    attempt one at getting deface to work

---
+++ app/overrides/add_currency_conversion_rate_to_general_settings.rb
@@ -0,0 +1,5 @@
+Deface::Override.new(:virtual_path => "admin/configurations/_form",
+                     :name => "add_currency_conversion_rate_to_general_settings",
+                     :insert_after => "input",
+                     :disabled => false,
+                     :text => "your mum and i kissed")
