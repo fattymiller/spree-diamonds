1c7e7716a71e3e82299263dbee9329c5262b4d95
Fatty <fattymiller@gmail.com>
2013-04-02 15:24:35 +1000

    defaulting sale information in the adminstration panel

--- app/overrides/add_sale_info_to_product.rb
+++ app/overrides/add_sale_info_to_product.rb
@@ -4,16 +4,16 @@ Deface::Override.new(:virtual_path => "spree/admin/products/_form",
                      :text => "
                      <p class=\"\" id=\"product_sale_price_field\">
                        <label for=\"product_sale_price\">Sale Price</label><br>
-                       <input id=\"product_sale_price\" name=\"product[sale_price]\" size=\"30\" type=\"text\">
+                       <input id=\"product_sale_price\" name=\"product[sale_price]\" size=\"30\" type=\"text\" value=\"<%= @product[:sale_price] %>\">
                      </p>
                      
                      <p class=\"\" id=\"product_sale_available_from_field\">
                        <label for=\"product_sale_available_from\">Sale starts</label><br>
-                       <input class=\"datepicker\" id=\"product_sale_available_from\" name=\"product[sale_starts]\" size=\"30\" type=\"text\">
+                       <input class=\"datepicker\" id=\"product_sale_available_from\" name=\"product[sale_starts]\" size=\"30\" type=\"text\" value=\"<%= @product[:sale_starts] %>\">
                      </p>
                      
                      <p class=\"\" id=\"product_sale_ends_field\">
                        <label for=\"product_sale_ends\">Sale ends</label><br>
-                       <input class=\"datepicker\" id=\"product_sale_ends\" name=\"product[sale_ends]\" size=\"30\" type=\"text\">
+                       <input class=\"datepicker\" id=\"product_sale_ends\" name=\"product[sale_ends]\" size=\"30\" type=\"text\" value=\"<%= @product[:sale_ends] %>\">
                      </p>")
  
