44fd3e1b62e932f75c9735b3115fa79044f48260
Fatty <fattymiller@gmail.com>
2013-06-14 15:48:01 +1000

    certificate changes

--- app/overrides/add_certification_upload_to_variant.rb
+++ app/overrides/add_certification_upload_to_variant.rb
@@ -1,6 +1,7 @@
 Deface::Override.new(:virtual_path => "spree/admin/variants/_form",
                      :name => "add_certification_upload_to_variant",
                      :insert_before => "[data-hook=\"sku\"]",
+                     :disabled => true,
                      :text => "
                        <%= f.label :diamond_certificate, 'Certificate file:' %><br />
                        <%= f.file_field :diamond_certificate %>")
@@ -8,4 +9,6 @@ Deface::Override.new(:virtual_path => "spree/admin/variants/_form",
 Deface::Override.new(:virtual_path => "spree/admin/variants/_form",
                      :name => "add_certification_download_link_to_variant",
                      :insert_before => "[data-hook=\"sku\"]",
+                     :disabled => true,
                      :text => "<p><%= link_to \"View current certificate\", f.object.diamond_certification.images.first.attachment.url(:original), :target => \"_blank\" if f.object.diamond_certification && f.object.diamond_certification.images.first %></p>")
+
